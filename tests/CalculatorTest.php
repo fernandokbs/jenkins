b096029c055cba49412d014c3b989e661dd786ef
Fernando <fernando93d@gmail.com>
2024-04-13 12:09:54 -0600

    asdf

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -10,4 +10,10 @@ class CalculatorTest extends TestCase
         $calculator = new Calculator();
         $this->assertEquals(5, $calculator->add(2, 3), "Adding 2 and 3 should return 5");
     }
+
+    public function testAdd2()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(10, $calculator->add(5, 5), "Adding 5 and 5 should return 10");
+    }
 }
